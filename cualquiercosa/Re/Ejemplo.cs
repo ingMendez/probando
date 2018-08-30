45eceb1f82afec94136051c98f4aed96de39a591
Reydi Alejandro Mendez Suarez <raydi_mendez@ucne.edu.do>
2018-08-30 10:53:15 -0400

    Provando

---
+++ cualquiercosa/Re/Ejemplo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cualquiercosa.Re
+{
+    class Ejemplo
+    {
+        public void Ej()
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.ReadKey();
+        }
+    }
+}
